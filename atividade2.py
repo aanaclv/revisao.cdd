693c3b7f692e960850f20b1354ac301c50fc0f95
ana clara <anaclaraalves675@gmail.com>
2025-04-17 14:05:44 -0300

    atividade2

---
+++ atividade2.py
@@ -0,0 +1,10 @@
+n = int(input("Digite um valor: "))
+if n % 2 == 0:
+    print(f"{n} é par")
+else:
+    print(f"{n} é ímpar")
+if n < 0:
+    print(f"{n} é negativo")
+else:
+    print(f"{n} é positvo")
+
