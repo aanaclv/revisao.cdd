32df2bbb0a24357ebde0f93873fd7464d269ad17
ana clara <anaclaraalves675@gmail.com>
2025-04-17 14:46:07 -0300

    atividade4

---
+++ atividade4.py
@@ -0,0 +1,5 @@
+salariomin = float(input("Insira o salário mínimo: "))
+salario = float(input("Insira seu salário: "))
+
+qtsalariomin = (salario/salariomin)
+print(f"{qtsalariomin:.0f} salários mínimos")
