43a57d68cd969b9e65757bfef0ecebe8b423ff1c
ana clara <anaclaraalves675@gmail.com>
2025-04-17 14:29:15 -0300

    atividade2 + while

--- atividade2.py
+++ atividade2.py
@@ -1,10 +1,14 @@
-n = int(input("Digite um valor: "))
-if n % 2 == 0:
-    print(f"{n} é par")
-else:
-    print(f"{n} é ímpar")
-if n < 0:
-    print(f"{n} é negativo")
-else:
-    print(f"{n} é positvo")
+novovalor = "s"
+while novovalor == "s":
+    n = int(input("Digite um valor: "))
 
+    if n % 2 == 0:
+        print(f"{n} é par")
+    else:
+        print(f"{n} é ímpar")
+    if n < 0:
+        print(f"{n} é negativo e par")
+    else:
+        print(f"{n} é positvo")
+
+    novovalor=input("Deseja fazer o cálculo novamente? (s/n)")
